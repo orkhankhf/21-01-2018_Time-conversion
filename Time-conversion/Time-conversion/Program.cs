0e2ad1cb2b08b2ec45408f166a4e61ba91897381
Orkhan Farajov <orkhankhf@code.edu.az>
2018-01-21 17:52:04 +0400

    My solution

---
+++ Time-conversion/Time-conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Time_conversion
+{
+    class Program
+    {
+        static string timeConversion(string s)
+        {
+            DateTime time = Convert.ToDateTime(s);
+            return time.ToString("HH:mm:ss");
+        }
+
+        static void Main(String[] args)
+        {
+            string s = Console.ReadLine();
+            string result = timeConversion(s);
+            Console.WriteLine(result);
+        }
+    }
+}
